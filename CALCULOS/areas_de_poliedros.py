58bf275fa1422072f27f13d4d6b6ed31375fdcf8
lucavinic <lucavinic2@hotmail.com>
2023-04-15 11:28:17 -0300

    novo código

--- CALCULOS/areas_de_poliedros.py
+++ CALCULOS/areas_de_poliedros.py
@@ -1,36 +1,36 @@
-print('Exemplo de polígonos: cubo, prisma, pirâmide, esfera, cilindro')
+print('Exemplo de poliedros: cubo, prisma, pirâmide, esfera, cilindro')
 a = input('informe qual poliedro deseja calcular a área: ')
 while True:
     if a == 'cubo':
         b = float(input('qual o lado do cubo? '))
         d = 6 * b**2
-        print('A área do cubo é = ', d)
+        print(f'A área do cubo é = {d:.2f}')
         break
     elif a == 'prisma':
         e = float(input('qual o perímetro da base do prisma? '))
         c = float(input('qual a base do prisma? '))
         f = float(input('qual a altura do prisma? '))
         w = 2*c + (e * f)
-        print('A área do prisma é = ', w)
+        print(f'A área do prisma é = {w:.2f}')
         break
     elif a == 'pirâmide':
         h = float(input('qual o perímetro da base da pirâmide? '))
         g = float(input('qual o valor do apótema da pirâmide? '))
         i = float(input('qual a área da base da pirâmide? '))
         z = (h * g)/2 + (i)
-        print('A área da pirâmide é = ', z)
+        print(f'A área da pirâmide é = {z:.2f}')
         break
     elif a == 'esfera':
         j = float(input('qual o raio da esfera? '))
         l = 4 * 3.14 * j**2
-        print('A área da esfera é = ', l)
+        print(f'A área da esfera é = {l:.2f}')
         break
     
     elif a == 'cilindro':
         m = float(input('qual o raio do cilindro? '))
         n = float(input('qual a altura do cilindro? '))
         p = (2 * 3.14 * m**2) + (2 * 3.14 * m * n)
-        print('A área do trapézio é = ', p)
+        print(f'A área do cilindro é = {p:.2f}')
         break
     
     else:
