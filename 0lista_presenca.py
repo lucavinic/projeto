02d49737bc49f817feda2f3a0cb24ed8e310e0c6
lucavinic <lucavinic2@hotmail.com>
2023-05-22 22:08:53 -0300

    novos códigos

--- 0lista_presenca.py
+++ 0lista_presenca.py
@@ -2,38 +2,36 @@ import sqlite3
 conexao = sqlite3.connect('tabela_presenca.sqlite3')
 cursor = conexao.cursor()
 
-'''comando_sql = f""" CREATE TABLE Cadastro(
+comando_sql = f""" CREATE TABLE if not exists Cadastro(
                     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                     nome VARCHAR(150),
                     cargo VARCHAR(50),
-                    data VARCHAR(25),                    
+                    data VARCHAR(25), 
+                    ch INTEGER,                  
                     FOREIGN KEY(data) REFERENCES PF(status)
                 );"""
 cursor.execute(comando_sql)
 
-comando_sql = f""" CREATE TABLE Reuniao(
+comando_sql = f""" CREATE TABLE if not exists Reuniao(
                     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                     duracao INTEGER,
+                    horario VARCHAR(25),
                     data  VARCHAR(25),
                     FOREIGN KEY(duracao) REFERENCES Carga_Horaria(hc)
                 );"""
 cursor.execute(comando_sql)
 
-comando_sql = f""" CREATE TABLE PF(
+comando_sql = f""" CREATE TABLE if not exists PF(
                     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                    nome VARCHAR(150),
-                    data VARCHAR(25),
+                    id_integ INTEGER,
+                    id_reuniao INTEGER,
                     status VARCHAR(20),
-                    FOREIGN KEY(status) REFERENCES Carga_Horaria(hc)
+                    FOREIGN KEY(id_integ) REFERENCES Cadastro(id),
+                    FOREIGN KEY(id_reuniao) REFERENCES Reuniao(id)
                 );"""
 cursor.execute(comando_sql)
 
-comando_sql = f""" CREATE TABLE Carga_Horaria(
-                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                    nome VARCHAR(150),
-                    hc INTEGER
-                );"""
-cursor.execute(comando_sql) '''
+
 
 def cadastrar_integrantes(conexao):
     cursor = conexao.cursor()
@@ -43,8 +41,8 @@ def cadastrar_integrantes(conexao):
     print("Use esse modelo para a data: dd/mm/yyyy")
     data = input("Informe a data do cadastro do(a) integrante: ")
 
-    comando_sql = f"INSERT INTO Cadastro(nome, cargo, data) VALUES (?, ?, ?)"
-    values = [nome, cargo, data]
+    comando_sql = f"INSERT INTO Cadastro(nome, ch, cargo, data) VALUES (?, ?, ?, ?)"
+    values = [nome, 0,  cargo, data]
     cursor.execute(comando_sql, values)
     conexao.commit()
 
@@ -52,43 +50,68 @@ def cadastrar_tempo_reuniao(conexao):
     cursor = conexao.cursor()
 
     duracao = int(input("Informe(em minutos) a duração da reunião: "))
+    print("Use esse modelo para as horas: hh:mm")
+    horario = input("Informe o horário da reunião: ")
     print("Use esse modelo para a data: dd/mm/yyyy")
     data = input("Informe a data da reunião: ")
 
-    comando_sql = f"INSERT INTO Reuniao(duracao, data) VALUES (?, ?)"
-    values = [duracao, data]
+    comando_sql = f"INSERT INTO Reuniao(duracao, horario, data) VALUES (?, ?, ?)"
+    values = [duracao, horario, data]
     cursor.execute(comando_sql, values)
     conexao.commit()
 
 def cadastrar_pf(conexao):
     cursor = conexao.cursor()
+    listar_nomes_cadastrados(conexao)
 
-    nome = input("Informe o nome do(a) integrante: ")
-    print("Use esse modelo para a data: dd/mm/yyyy")
-    data = input("Informe a data da reunião atual: ")
-    print("")
-    status = input('Informe o status, se esse integrante teve presença ou falta nessa reunião: ')
+    id_integ = int(input("Informe o id do(a) integrante: "))
+    listar_reunioes_cadastrados(conexao)
 
-    comando_sql = f"INSERT INTO PF(nome, data, status) VALUES (?, ?, ?)"
-    values = [nome, data, status]
+    id_reuniao = int(input("Informe o id da reunião: "))
+    status = ''
+    while status not in['presente', 'falta']:
+        print("Digite 'presente' ou 'falta'!")
+        status = input('Informe o status, se esse integrante teve presença ou falta nessa reunião: ').lower()
+
+    comando_sql = f"INSERT INTO PF(id_integ, id_reuniao, status) VALUES (?, ?, ?)"
+    values = [id_integ, id_reuniao, status]
     cursor.execute(comando_sql, values)
+    calculo_ch(conexao, id_integ, id_reuniao, status)
     conexao.commit()
 
-def cadastrar_ch(conexao):
+def calculo_ch(conexao, id_integ, id_reuniao, status):
     cursor = conexao.cursor()
+    if status == 'presente':
+        cursor.execute(f"SELECT duracao FROM Reuniao WHERE id = {id_reuniao}")
+        tempo = cursor.fetchall()[0][0]
+        
+        cursor.execute(f"SELECT ch FROM Cadastro WHERE id = {id_integ}")
+        tempo_anterior = cursor.fetchall()[0][0]
+        
 
-    nome = input("Informe o nome do(a) integrante: ")
+        cursor.execute(f"UPDATE Cadastro SET ch = {tempo_anterior + tempo} WHERE id = {id_integ}")
+        conexao.commit()
+
+
+def listar_reunioes_cadastrados(conexao):
+    cursor = conexao.cursor()
+    comando_sql = f"""SELECT * FROM Reuniao"""
+    dados = cursor.execute(comando_sql)
+    for dado in dados:
+        print(dado)
 
 def listar_nomes_cadastrados(conexao):
     cursor = conexao.cursor()
-    comando_sql = f"""SELECT * FROM Cadastro WHERE nome"""
+    comando_sql = f"""SELECT * FROM Cadastro"""
+    dados = cursor.execute(comando_sql)
+    for dado in dados:
+        print(dado)
     
-    cursor.execute(comando_sql)
 
 
 while True:
     print('---SISTEMA DE PRESENÇA DAS REUNIÕES---')
-    print('\n 1) Cadastrar Integrantes', '\n 2) Informar duração reunião', '\n 3) Cadastrar Presença ou Falta', '\n 4) Calcular Horário Complementar Individual', '\n 5) Listar nome(s) já cadastrado', '\n 0) Sair')
+    print('\n 1) Cadastrar Integrantes', '\n 2) Informar duração reunião', '\n 3) Cadastrar Presença ou Falta', '\n 4) Listar nome(s) já cadastrado', '\n 0) Sair')
     opcao = int(input("O que você deseja?: "))
 
     if opcao == 0:
@@ -102,11 +125,8 @@ while True:
 
     elif opcao == 3:
         cadastrar_pf(conexao)
-
-    elif opcao == 4:
-        cadastrar_ch(conexao)
     
-    elif opcao == 5:
+    elif opcao == 4:
         listar_nomes_cadastrados(conexao)
 
     
---
+++ testeclasses.py
@@ -0,0 +1,21 @@
+
+class Cachorros:
+    def __init__(self, nome: str, raca: str, comprimento: float, peso: float):
+        self.nome
+        self.raca
+        self.comprimento
+        self.peso
+    
+    def latir(self):
+        print(f"Au au au!")
+
+    def morder(self):
+        print(f"O cachorro {self.nome} me mordeu!")
+
+    def comer(self):
+        print(f"Meu nome é {self.nome}, sou da raça {self.raca}, peso {self.peso}, e tenho um comprimento de {self.comprimento}, amo me alimentar!")
+
+atena = Cachorros('Atena', 'Pitbull', 45, 23)
+atena
+
+
