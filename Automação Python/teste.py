8d52d6b998722a5f55f5cb4a11bc46d29bb118fa
lucavinic <lucavinic2@hotmail.com>
2023-09-26 13:13:31 -0300

    novos códigos

--- Automação Python/teste.py
+++ Automação Python/teste.py
@@ -1,10 +1,6 @@
-'''import pyautogui
+import pyautogui
 import time
 
 time.sleep(5)
 
-print(pyautogui.position())'''
-
-import pandas as pd
-tabela = pd.read_excel("produtos.xlsx")
-print(tabela)
+print(pyautogui.position())
