7d772599876c35efe0a3d5e52d1562163efa758d
lucavinic <lucavinic2@hotmail.com>
2024-03-11 21:41:16 -0300

    selenium

--- selenium.py
+++ selenium.py
@@ -1,11 +1,13 @@
-import pyautogui
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time 
 
-py = pyautogui
+
 
 # Inicialize o WebDriver do Chrome
 servico = Service(ChromeDriverManager().install())
@@ -15,27 +17,109 @@ navegador.maximize_window()
 # Navegue até o site
 navegador.get('https://apps.fas.usda.gov/psdonline/app/index.html#/app/advQuery')
 
-# Defina a lista de nomes usando o XPath fornecido
-nomes_xpath = '/html/body/div[2]/div[3]/div/div[2]/div[2]/div[1]/select'
-market_years24 = '/html/body/div[2]/div[3]/div/div[2]/div[2]/div[4]/select/option[1]'
-market_years23 = '/html/body/div[2]/div[3]/div/div[2]/div[2]/div[4]/select/option[2]'
-countries = '/html/body/div[2]/div[3]/div/div[2]/div[4]/div[3]/select/option[2]'
+def selecionar_commodity(navegador, commodity_index):
+    # Espera até que o elemento <select> seja carregado e visível
+    elemento_select = WebDriverWait(navegador, 20).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "select[ng-model='vm.criteria.commodities']"))
+    )
+    
+    
+    # Cria uma instância de Select usando o elemento encontrado
+    select = Select(elemento_select)
+    
+    # Seleciona a opção pelo texto visível
+    #select.select_by_visible_text(commodity_name)
+    select.select_by_index(commodity_index)
+    
+def selecionar_atributos(navegador):
+    # Espera até que o elemento <select> seja carregado e visível
+    elemento_select = WebDriverWait(navegador, 20).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "select[ng-model='vm.criteria.attributes']"))
+    )
+    
+    
+    # Cria uma instância de Select usando o elemento encontrado
+    select = Select(elemento_select)
+    
+    # Seleciona a opção pelo texto visível
+    #select.select_by_visible_text(commodity_name)
+    select.select_by_value("number:88")
+    
+    
+def selecionar_market(navegador):
+    # Espera até que o elemento <select> seja carregado e visível
+    elemento_select = WebDriverWait(navegador, 20).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "select[ng-model='vm.criteria.marketYears']"))
+    )
+    
+    
+    # Cria uma instância de Select usando o elemento encontrado
+    select = Select(elemento_select)
+    
+    # Seleciona a opção pelo texto visível
+    #select.select_by_visible_text(commodity_name)
+    select.select_by_value("number:2024")
+    select.select_by_value("number:2023")
+    
+def selecionar_top(navegador):
+    # Localizar o checkbox pelo seletor CSS usando o valor do atributo ng-model
+    checkbox = navegador.find_element(By.CSS_SELECTOR, "input[type='checkbox'][ng-model='vm.criteria.chkTopCountry']")
 
-# Encontre o elemento select correspondente ao XPath
-select_element = navegador.find_element(By.XPATH, nomes_xpath)
-select_years1 = navegador.find_element(By.XPATH, market_years24)
-select_years2 = navegador.find_element(By.XPATH, market_years23)
-select_country = navegador.find_element(By.XPATH, countries)
-
-# Obtenha a lista de opções desse elemento select
-opcoes = select_element.find_elements(By.TAG_NAME, 'option')
-
-# Iterar sobre cada opção
-for opcao in opcoes:
-    # Clique nela para selecioná-la
-    opcao.click() 
-    py.click(x=675, y=515, clicks=2)
-    select_years.click()
-    select_years1.click()
-    select_years2.click()
-    countries.click()
+    # Clicar no checkbox
+    checkbox.click()
+    
+def selecionar_top5(navegador):
+    # Espera até que o elemento <select> seja carregado e visível
+    elemento_select = WebDriverWait(navegador, 20).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "select[ng-model='vm.criteria.topCountryCount']"))
+    )
+    
+    
+    # Cria uma instância de Select usando o elemento encontrado
+    select = Select(elemento_select)
+    
+    # Seleciona a opção pelo texto visível
+    #select.select_by_visible_text(commodity_name)
+    select.select_by_value("5")
+    
+def selecionar_run(navegador):
+    # Clique no botão usando a classe
+    run = '/html/body/div[2]/div[3]/div/div[2]/div[8]/div/button[1]'
+    select_run = navegador.find_element(By.XPATH, run)
+    select_run.click()
+    
+def selecionar_download(navegador):
+    # Clique no botão usando a classe
+    run = '/html/body/div[2]/div[3]/div/div[3]/div[1]/a[2]/i'
+    select_run = navegador.find_element(By.XPATH, run)
+    select_run.click()
+    
+def selecionar_back(navegador):
+    # Clique no botão usando a classe
+    run = '/html/body/div[2]/div[3]/div/div[3]/div[1]/a[1]/i'
+    select_run = navegador.find_element(By.XPATH, run)
+    select_run.click()
+    
+def selecionar_reset(navegador):
+    # Clique no botão usando a classe
+    run = '/html/body/div[2]/div[3]/div/div[2]/div[8]/div/button[2]'
+    select_run = navegador.find_element(By.XPATH, run)
+    select_run.click()
+    
+    
+    
+for index in range(62):
+    # Selecione o primeiro elemento pelo índice
+    selecionar_commodity(navegador, index)
+    time.sleep(1)
+    selecionar_atributos(navegador)
+    selecionar_market(navegador)
+    selecionar_top(navegador)
+    selecionar_top5(navegador)
+    time.sleep(1)
+    selecionar_run(navegador)
+    time.sleep(1)
+    selecionar_download(navegador)
+    selecionar_back(navegador)
+    time.sleep(1)
+    selecionar_reset(navegador)
